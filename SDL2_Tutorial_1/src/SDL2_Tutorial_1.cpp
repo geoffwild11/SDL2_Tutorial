435bf0c10b40fdbeae5eb9231063434ce0f17d1e
Geoff Wild <boris@zerohour-productions.net>
2014-01-20 10:52:51 -0600

    Getting Started

---
+++ SDL2_Tutorial_1/src/SDL2_Tutorial_1.cpp
@@ -0,0 +1,9 @@
+// SDL Learning from Lazy Foo Productions SDL 2
+// Geoff Wild
+
+#include <stdio.h>
+#include <SDL2\SDL.h>
+
+//Constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
